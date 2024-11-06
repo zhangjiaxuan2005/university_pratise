3190bade15061953a7a5e717b75d3f2090e88623
zhangjiaxuan2005 <1724640501@qq.com>
2024-11-06 14:52:45 +0800

    1.1

--- cpp/test/test1.cpp
+++ cpp/test/test1.cpp
@@ -4,8 +4,8 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int arr[100]={1,1};
-    for (int i = 0; i < 25; i++) {
+    long long arr[100]={1,1};
+    for (int i = 0; i < 50; i++) {
         arr[i+2]= arr[i]+arr[i+1];
         cout<<arr[i]<<" ";
     }
