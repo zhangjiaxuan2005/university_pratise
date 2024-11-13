48dfc9e5c4d3ea403a46f2b0afbea6c619fcecbf
zhangjiaxuan2005 <1724640501@qq.com>
2024-11-13 15:58:34 +0800

    1.1

--- cpp/test/main.cpp
+++ cpp/test/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
+const int N=109;
+int a[N];
 int main() {
-
+    int n;cin>>n;
+    for(int i=0;i<=n;++i){
+        cin>>a[n];
+    }
+    int count=0;
+    for(int i=0;i<=n;++i){
+        count+=a[n];
+    }
+    cout<<count;
     return 0;
 }
