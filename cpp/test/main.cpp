12b64a81a85cffaf3d180e75a364cda469962e5a
zhangjiaxuan2005 <1724640501@qq.com>
2024-12-02 17:00:23 +0800

    1.0.0

--- cpp/test/main.cpp
+++ cpp/test/main.cpp
@@ -1,15 +1,37 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
 int main() {
-    int n;cin>>n;
-    int a[n];
-    for(int i=0;i<=n;++i){
-        cin>>a[n];
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> nums(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
     }
-    int count=0;
-    for(int i=0;i<=n;++i){
-        count+=a[n];
+
+    vector<int> dp(k + 1, 0);
+    int maxGCD = 0;
+
+    for (int i = 0; i < n; ++i) {
+        vector<int> newDp(k + 1, 0);
+        for (int j = 0; j <= k; ++j) {
+            newDp[j] = dp[j];
+            if (j > 0 && nums[i] % dp[j - 1] == 0) {
+                newDp[j] = max(newDp[j], gcd(nums[i], dp[j - 1]));
+            }
+            maxGCD = max(maxGCD, newDp[j]);
+        }
+        dp = newDp;
     }
-    cout<<count;
+
+    // 输出调试信息
+    cout << "Final maxGCD: " << maxGCD << endl;
+
     return 0;
 }
