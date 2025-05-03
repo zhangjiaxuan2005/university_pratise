93d5c65c57acb37ba25f0881bfa7368dd2e5de81
zhangjiaxuan2005 <1724640501@qq.com>
2025-05-03 14:50:15 +0800

    初始化仓库

--- cpp/untitled/cmake-build-debug/cmake_install.cmake
+++ cpp/untitled/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: E:/projects/cpp
+# Install script for directory: E:/projects/cpp/untitled
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/cpp")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/untitled")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/projects/cpp/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/projects/cpp/untitled/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
