8ab08bf241fbc5f5797b2c7b17301df8caff86e1
zhangjiaxuan2005 <1724640501@qq.com>
2025-06-19 19:32:55 +0800

    C语言第四天，C++第四天，java第一天

---
+++ cpp/Base_class/Day_four/array_one.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 17246 on 2025/6/19.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[10];
+    for (int i = 0; i < 10; i++) {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i]<<" ";
+    }
+}
