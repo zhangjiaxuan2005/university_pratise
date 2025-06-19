aaea0a0e4d20e4a8f214fb0b382b3746b794d9fd
zhangjiaxuan2005 <1724640501@qq.com>
2025-06-19 19:14:29 +0800

    c++第三天

--- cpp/untitled/Day_three/controller_four.cpp
+++ cpp/untitled/Day_three/controller_four.cpp
@@ -5,5 +5,11 @@
 using namespace std;
 
 int main() {
-
+    int sum = 0;
+    int count = 1;
+    while (count <= 100) {
+        sum += count;
+        count++;
+    }
+    cout << sum << endl;
 }
