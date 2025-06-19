700614821c49f1de96b6336c684deb6bd30c13fc
zhangjiaxuan2005 <1724640501@qq.com>
2025-06-19 15:00:28 +0800

    c++第一天

--- cpp/untitled/Day_one/constant.cpp
+++ cpp/untitled/Day_one/constant.cpp
@@ -1,10 +1,11 @@
 //
-// Created by 17246 on 2025/6/15.
+// Created by 17246 on 2025/6/19.
 //
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    cout << "Hello world" << endl;
+    cout<<1<<3.14<<'c'<<"zjx"<<endl;
     return 0;
 }
