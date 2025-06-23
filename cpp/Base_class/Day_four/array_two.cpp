d8a3c753320eb2c9780f21c3b2fbc4ab1a481ff7
zhangjiaxuan2005 <1724640501@qq.com>
2025-06-23 16:03:51 +0800

    仓颉第一天

--- cpp/Base_class/Day_four/array_two.cpp
+++ cpp/Base_class/Day_four/array_two.cpp
@@ -1,3 +1,25 @@
 //
 // Created by 17246 on 2025/6/19.
 //
+#include <iostream>
+using namespace std;
+
+int main() {
+    char arr[] = "hello";
+    for (int i = 0; i < 6; i++) {
+        cout << arr[i];
+    }
+    cout << endl;
+    for (char i: arr) {
+        cout << i;
+    }
+    cout << endl;
+    int arr1[10][10];
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            arr1[i][j] = 0;
+            cout << arr1[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
--- java/Base_class/src/com/zjx_jason/www/Day_one/HelloWorld.java
+++ java/Base_class/src/com/zjx_jason/www/Day_one/HelloWorld.java
@@ -1,4 +1,7 @@
 package com.zjx_jason.www.Day_one;
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
