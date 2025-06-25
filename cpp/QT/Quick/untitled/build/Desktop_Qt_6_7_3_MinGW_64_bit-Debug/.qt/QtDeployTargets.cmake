313c6c4769313afa523a55f3adc923e0cd4a22b5
zhangjiaxuan2005 <1724640501@qq.com>
2025-06-25 10:50:06 +0800

    refactor(cmake): 更新 CMakeLists.txt 中的可执行文件路径
    - 将 Scanf、Conversion、Operator_three、controller_three 和 controller_four 的源文件路径更新
    - 从 Day_two 和 Day_three 移动到 Day_one 目录
    - 删除了 Day_four 目录下的 controller_three 和 controller_four

---
+++ cpp/QT/Quick/untitled/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_appuntitled_FILE E:/projects/cpp/QT/Quick/untitled/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/appuntitled.exe)
+set(__QT_DEPLOY_TARGET_appuntitled_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_appuntitled_RUNTIME_DLLS D:/Program_Files/Qt/6.7.3/mingw_64/bin/Qt6Quick.dll;D:/Program_Files/Qt/6.7.3/mingw_64/bin/Qt6QmlModels.dll;D:/Program_Files/Qt/6.7.3/mingw_64/bin/Qt6Qml.dll;D:/Program_Files/Qt/6.7.3/mingw_64/bin/Qt6Network.dll;D:/Program_Files/Qt/6.7.3/mingw_64/bin/Qt6OpenGL.dll;D:/Program_Files/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;D:/Program_Files/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
---
+++ cpp/QT/Quick/untitled/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/bin/qt_setup_tool_path.bat
@@ -0,0 +1,3 @@
+@echo off
+set PATH=D:\Program_Files\Qt\6.7.3\mingw_64\bin;%PATH%
+%*
